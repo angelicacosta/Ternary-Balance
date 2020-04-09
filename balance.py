cede6d9725db31e6f1fa559c4c7729b191f441c6
angelicacosta <14-10005@usb.ve>
2020-04-09 13:57:01 +0200

    Restructuring of the code and handling of invalid input values

--- balance.py
+++ balance.py
@@ -1,4 +1,5 @@
-#Angelica ACOSTA ARTETA
+# Author
+# Angelica ACOSTA ARTETA
 
 #Function that returns the result of adding all the elements of an array 
 def summing(array):
@@ -9,69 +10,104 @@ def summing(array):
 
 #Function that returns a string made of all the elements of an array 
 def stringarray(array):
-	string=""
-	for x in range(len(array)):
-		string+=str(array[x])+" "
+	try:
+		string=str(array[0])
+		for x in range(1,len(array)):
+			string+=", "+str(array[x])
+	except:
+		string=""
+	
 	return string
 
 #Function that determines the minimum weight needed to represent n
 def need(n):
-	total=1
-	top=1
-	i=0
-	while total<n:
-		top*=3
-		total+=top
-		i+=1	
-	return(top, i)
-
-print("Please write how much you want to weight: ")
-inp=input()
-inp=int(inp)
-current, i=need(inp)
-
-add=[]				#Right side of the balance
-sub=[inp]			#Left side of the balance
-missing=inp			#How much do we need to weight
-substracting=False	#Variable that represents if we have weighted more than needed
-
-#Loop that goes from the maximum weight needed to the smallest one
-for j in range(i+1):
-
-	#If we already finished weighting we exit the loop
-	if missing==0:
-		break
-
-	#If the current weight is bigger than the maximum needed we skip 
-	elif need(missing)[0]<current:
-		current=int(current/3)
-		pass	
-
-	#If we weighted more than needed
-	if substracting:
-		sub.append(current) #We add the current weight to the left side of the balance
-
-		#If we need to keep weighting on the right
-		if summing(sub)>summing(add):
-			substracting=False
-			missing=summing(sub)-summing(add)
-
-		else:
-			missing=summing(add)-summing(sub)
-
-	#If we have not weighted enough
+	if n>0:
+		total=1
+		top=1
+		i=0
+		while total<n:
+			top*=3
+			total+=top
+			i+=1	
+		return(top, i)
 	else:
-		add.append(current) #We add the current weight to the right side of the balance
+		return(None,None)
 
-		#If we need to keep weighting on the left
-		if summing(add)>summing(sub):
-			substracting=True
-			missing=summing(add)-summing(sub)		
-		else:
-			missing=summing(sub)-summing(add)
+def weighting(user_input):
 	
-	current=int(current/3) #We obtain the next smaller weight
+	left_side 			= [user_input]
+	right_side			= []
+	missing_to_weight 	= user_input
+	substracting		= False
+	current_weight, current_power = need(user_input)
+	
+	# Loop that goes from the maximum weight needed (current) to the smallest one
+	while(current_power>=0):
 
-#We print both sides of the balance
-print("right: "+stringarray(add))
-print("left: "+stringarray(sub))
+		# If we already finished weighting we exit the loop
+		if missing_to_weight==0:
+			break
+
+		# If the current weight is bigger than the maximum needed to weigth current then we skip 
+		elif need(missing_to_weight)[0]<current_weight:
+			current_weight, current_power=need(missing_to_weight) # The current weight will now be the maximum needed that
+									# corresponds to 3 to the power of i
+			continue	
+
+		# If we weighted more than needed
+		if substracting:
+			left_side.append(current_weight) # We add the current weight to the left side of the balance
+
+			# If we need to keep weighting on the right
+			if summing(left_side)>summing(right_side):
+				substracting=False
+				missing_to_weight=summing(left_side)-summing(right_side)
+
+			else:
+				missing_to_weight=summing(right_side)-summing(left_side)
+
+		# If we have not weighted enough
+		else:
+			right_side.append(current_weight) # We add the current weight to the right side of the balance
+
+			# If we need to keep weighting on the left
+			if summing(right_side)>summing(left_side):
+				substracting=True
+				missing_to_weight=summing(right_side)-summing(left_side)		
+			else:
+				missing_to_weight=summing(left_side)-summing(right_side)
+		
+		current_weight=int(current_weight/3) # We obtain the next smaller weight
+		current_power-=1					 # That corresponds to the next smaller power 
+	
+	return (left_side, right_side)
+
+
+if __name__ == '__main__':
+	while True:
+	# We ask the user to specify how much would he like to weight
+		inp=input("Please write how much you want to weight: ")
+
+		# We try to convert the input into an integer
+		try:
+			inp=int(inp)
+
+			# If the input is negative or 0 we'll display an error message
+			# and continue the loop to ask for a number again
+			if inp<1:
+				print("Error: the input must be a positive integer. Please try again.\n")
+
+			# However if the input is a positive integer we can weight
+			# and exit the loop
+			else:
+				break
+	
+		# If the input is not an integer then we'll display an error message
+		# and continue the loop to ask for a number again
+		except ValueError:
+			print("Error: the input must be a positive integer. Please try again.\n")
+
+	sub, add = weighting(inp)
+
+	print("left: "+stringarray(sub))
+	print("right: "+stringarray(add))
